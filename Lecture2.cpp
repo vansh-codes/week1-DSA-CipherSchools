fb150c6f4174bc4c91d28ed10ee013650a5ccf20
Vansh Chaurasiya <114163734+vansh-codes@users.noreply.github.com>
2023-10-02 23:00:42 +0530

    Add files via upload

---
+++ Lecture2.cpp
@@ -0,0 +1,10 @@
+    #include<iostream>     //header file
+    using namespace std;
+
+    int main()    //main program
+        {
+        	
+            cout<<"Hello, World!";   //printing 
+            return 0;   //exit program
+            
+        }
